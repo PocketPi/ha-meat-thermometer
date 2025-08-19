10c84d4c75744be4c85d36ccabb38849aaa5162d
Peter Rasmussen <peter@rustos.org>
2025-08-19 23:07:58 +0200

    add web front

--- main/main.c
+++ main/main.c
@@ -1,9 +1,34 @@
 #include "esp_log.h"
 #include "nvs_flash.h"
+#include "mdns.h"
+#include "lwip/apps/netbiosns.h"
+#include "esp_littlefs.h"
 #include "wifi.h"
 #include "wifi_scan.h"
 
 static const char *TAG = "main";
+
+#define MDNS_INSTANCE "esp home web server"
+
+#define FS_MOUNT_POINT "/www"
+#define MDNS_HOST_NAME "dashboard"
+
+esp_err_t start_rest_server(const char *base_path);
+
+static void initialise_mdns(void)
+{
+    mdns_init();
+    mdns_hostname_set(MDNS_HOST_NAME);
+    mdns_instance_name_set(MDNS_INSTANCE);
+
+    mdns_txt_item_t serviceTxtData[] = {
+        {"board", "esp32"},
+        {"path", "/"}
+    };
+
+    ESP_ERROR_CHECK(mdns_service_add("ESP32-WebServer", "_http", "_tcp", 80, serviceTxtData,
+                                     sizeof(serviceTxtData) / sizeof(serviceTxtData[0])));
+}
 
 void app_main(void) {
     // Initialize NVS
@@ -13,6 +38,21 @@ void app_main(void) {
         ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
+
+    esp_vfs_littlefs_conf_t conf = {
+        .base_path = "/www",
+        .partition_label = "www",
+        .format_if_mount_failed = true,
+        .dont_mount = false,
+    };
+
+    esp_vfs_littlefs_register(&conf);
+    
+    initialise_mdns();
+    netbiosns_init();
+    netbiosns_set_name("esp32");
+
+    ESP_ERROR_CHECK(start_rest_server(FS_MOUNT_POINT));
 
     wifi_init();
 
